efb265509785d9315caf5905928a8b30bf4ebb77
ryanweiler92 <ryanweiler92@gmail.com>
2022-03-24 13:49:21 -0400

    update server.js

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/budget", {
   useNewUrlParser: true,
-  //useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
